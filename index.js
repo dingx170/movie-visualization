f1b460643ae9e4693f57980fa7b1556014f19147
Vindhya <vindhyanairlj@gmail.com>
2021-05-18 03:47:51 -0700

    Included chord chart with patterns, adjusted postions but the size of line and bar chart need to be changed so that labels of chord graph can be visible, pattern need to applied for other graphs too

--- index.js
+++ index.js
@@ -108,19 +108,27 @@ async function makeChart(movie, minutes) {
     }
 }
 
-async function createChord(movie, minutes) {
-  var data = await getData(movie, minutes);
+async function createChord(movie) {
+    var data = await d3.csv(movies[movie]);
     var width = parseInt($("#chordChartCanvas").parent().width()), height = width;
-    var innerRadius = width*0.65/2, outerRadius = width*0.70/2;
-    // create the svg area
-    var svg = d3.select('#chordChartCanvas')
-        .append("svg")
+    var innerRadius = width * 0.65 / 2, outerRadius = width * 0.70 / 2;
+    var names = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprised", "Neutral"];
+    var colors = ["#ff4000", "#ffbf00", "#A0522D", "#009933", "#5050FF", "#FF50FF", "#c2c2d6"];
+
+    // Get place holder for the svg area
+    var canvas = d3.select('#chordChartCanvas');
+
+    // Create patterns on the placeholder
+    createPatterns(canvas, colors);
+
+    // Create SVG area for chart
+    var svg = canvas.append("svg")
         .attr("width", width)
         .attr("height", height)
         .append("g")
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-    var names = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprised", "Neutral"];
-    var colors = ["#ff4000", "#ffbf00", "#A0522D", "#009933", "#5050FF", "#FF50FF", "#c2c2d6"];
+
+    // format the data
     completeMatrix = prepareData(data);
 
     // give this matrix to d3.chord(): it will calculates all the info we need to draw arc and ribbon
@@ -135,7 +143,7 @@ async function createChord(movie, minutes) {
     svg.selectAll("path")
         .data(res.groups)
         .enter().append("path")
-        .style("fill", function (d) { return colors[d.index]; })
+        .style("fill", function (d) { return getPattern(d.index); })
         .style("stroke", function (d) {
             return d3.rgb(colors[d.index]).darker()
         })
@@ -189,12 +197,7 @@ async function createChord(movie, minutes) {
         .enter().append("path")
         .attr("d", d3.ribbon().radius(innerRadius))
         .style("fill", function (d) {
-            var chordcolor = d3.scaleLinear()
-                .range([colors[d.target.index], colors[d.source.index]])
-                .domain([-1, 1])
-                .interpolate(d3.interpolateLab);
-            var weight = 1; //(d.source.value - d.target.value) / (d.source.value + d.target.value);
-            return chordcolor(weight);
+            return getPattern(d.source.index);
         })
         .style("stroke", function (d) {
             return d3.rgb(colors[d.source.index]).darker()
@@ -304,9 +307,81 @@ function prepareData(data) {
     return completeMatrix;
 }
 
+function createPatterns(canvas, colors) {
+    defs = canvas.append('svg').attr('width', 1).attr('height', 1).append('defs')
+
+    defs.append('pattern')
+        .attr('id', 'diagonalHatch')
+        .attr('patternUnits', 'userSpaceOnUse')
+        .attr('width', 4).attr('height', 6)
+        .append('path').attr('d', 'M-1, 1 12, -2 M0, 4 14, -4 M3, 5 12, -2')
+        .attr('stroke', colors[0]).attr('stroke-width', 1);
+
+    defs.append('pattern')
+        .attr('id', 'diagonalHatch1')
+        .attr('patternUnits', 'userSpaceOnUse')
+        .attr('width', 4).attr('height', 4)
+        .append('circle').attr('cx', 2).attr('cy', 2).attr('r', 2)
+        .attr('stroke', colors[1]).attr('stroke-width', 1);
+
+    defs.append('pattern')
+        .attr('id', 'diagonalHatch2')
+        .attr('patternUnits', 'userSpaceOnUse')
+        .attr('width', 4).attr('height', 4)
+        .append('rect').attr('width', 3).attr('height', 1)
+        .attr('stroke', colors[2]).attr('stroke-width', 1);
+
+    defs.append('pattern')
+        .attr('id', 'diagonalHatch3')
+        .attr('patternUnits', 'userSpaceOnUse')
+        .attr('width', 2).attr('height', 2)
+        .append('path').attr('d', 'M0 0l5 3v5l-5 -3z')
+        .attr('stroke', colors[3]).attr('stroke-width', 1);
+
+    defs.append('pattern').attr('width', 2).attr('height', 2)
+        .attr('id', 'diagonalHatch4')
+        .attr('patternUnits', 'userSpaceOnUse')
+        .attr('width', 4).attr('height', 4)
+        .append('circle').attr('cx', 0).attr('cy', 0).attr('r', 2)
+        .attr('stroke', colors[4]).attr('stroke-width', 1);
+
+    defs.append('pattern').attr('width', 2).attr('height', 2)
+        .attr('id', 'diagonalHatch5')
+        .attr('patternUnits', 'userSpaceOnUse')
+        .attr('width', 4).attr('height', 4)
+        .append('rect').attr('width', 1).attr('height', 3)
+        .attr('stroke', colors[5]).attr('stroke-width', 1);
+
+    defs.append('pattern').attr('width', 2).attr('height', 2)
+        .attr('id', 'diagonalHatch6')
+        .attr('patternUnits', 'userSpaceOnUse')
+        .attr('width', 2).attr('height', 4)
+        .append('path').attr('d', 'M-1, 1 2, -2 M0, 4 14, -4 M3, 5 12, -2')
+        .attr('stroke', colors[6]).attr('stroke-width', 2);
+}
+
+function getPattern(idx) {
+    switch (idx) {
+        case 0:
+            return 'url(#diagonalHatch)';
+        case 1:
+            return 'url(#diagonalHatch1)';
+        case 2:
+            return 'url(#diagonalHatch2)';
+        case 3:
+            return 'url(#diagonalHatch3)';
+        case 4:
+            return 'url(#diagonalHatch4)';
+        case 5:
+            return 'url(#diagonalHatch5)';
+        case 6:
+            return 'url(#diagonalHatch6)';
+    }
+}
+
 async function start()
 {
   await makeChart("mask", 20);
-  createChord("mask", 1);
+  createChord("mask");
 }
 start()
